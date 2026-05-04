fcfe5c0acbf6a7a07b7dc23bfdd5d8dc68fbb989
wesbragagt <wbraga@implentio.com>
2026-05-03 23:33:03 -0500

    fix: rename vaapiVdpau and noto-fonts-emoji to current names

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -5,7 +5,7 @@
       nerd-fonts.jetbrains-mono
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
     ];
     fontconfig.defaultFonts = {
       monospace = [ "JetBrainsMono Nerd Font" ];
