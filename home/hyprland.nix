4e76adf00e5ab27d9e806da877c42ecfad056349
wesbragagt <wbraga@implentio.com>
2026-05-05 12:03:18 -0500

    feat: remove nwg-dock autohide, add workspace binds 6-9

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -124,7 +124,7 @@
         "wl-paste --watch cliphist store"
         "waybar"
         "nm-applet --indicator"
-        "nwg-dock-hyprland -p bottom -lp end -i 36 -c rofi-freq -d"
+        "nwg-dock-hyprland -p bottom -lp end -i 36 -c rofi-freq"
         "swaync"
         "swww-daemon"
       ];
@@ -198,6 +198,14 @@
         "\$mod SHIFT, 3, movetoworkspace, 3"
         "\$mod SHIFT, 4, movetoworkspace, 4"
         "\$mod SHIFT, 5, movetoworkspace, 5"
+        "\$mod, 6, workspace, 6"
+        "\$mod, 7, workspace, 7"
+        "\$mod, 8, workspace, 8"
+        "\$mod, 9, workspace, 9"
+        "\$mod SHIFT, 6, movetoworkspace, 6"
+        "\$mod SHIFT, 7, movetoworkspace, 7"
+        "\$mod SHIFT, 8, movetoworkspace, 8"
+        "\$mod SHIFT, 9, movetoworkspace, 9"
         "\$mod SHIFT, BackSpace, exec, waypaper"
         "\$mod SHIFT, minus, exec, pkill -35 nwg-dock-hyprland"
       ];
