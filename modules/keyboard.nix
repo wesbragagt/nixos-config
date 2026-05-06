8ac571eb0d52ee6a526d56a496ce5e6d26de99c8
wesbragagt <40429790+wesbragagt@users.noreply.github.com>
2026-05-06 18:33:25 -0500

    chore: keyboard adjust

--- modules/keyboard.nix
+++ modules/keyboard.nix
@@ -1,17 +1,13 @@
-{ pkgs, ... }:
-let
-  it = pkgs.interception-tools;
-  c2e = pkgs.interception-tools-plugins.caps2esc;
-in
+{ ... }:
 {
-  services.interception-tools = {
+  services.keyd = {
     enable = true;
-    plugins = [ c2e ];
-    udevmonConfig = ''
-      - JOB: "${it}/bin/intercept -g $DEVNODE | ${c2e}/bin/caps2esc | ${it}/bin/uinput -d $DEVNODE"
-        DEVICE:
-          EVENTS:
-            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
-    '';
+    keyboards.default = {
+      ids = [ "*" ];
+      settings.main = {
+        capslock = "overload(control, esc)";
+        esc = "esc";
+      };
+    };
   };
 }
